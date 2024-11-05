0bdc3df946dba0474dc892881def55d9ee3d4e91
Ccgomez2 <ccasgom@upv.es>
2024-11-05 11:17:36 +0100

    Implementación QuickSort

--- DyV.h
+++ DyV.h
@@ -1,7 +1,10 @@
 #include <vector>
 #include <iostream>
+using namespace std;
 
-int BusquedaBinaria(int x, const std::vector<int>& v, int ini, int fin) {
+template <typename T>
+
+int BusquedaBinaria(int x, const vector<T>& v, int ini, int fin) {
         
 	if (ini > fin) { // CASO BASE
 		return -1;
@@ -18,3 +21,28 @@ int BusquedaBinaria(int x, const std::vector<int>& v, int ini, int fin) {
 	}
 }
 
+template <typename T>
+
+int Partition(vector<T>& v, int ini, int fin) {
+	T x = v[fin];
+	int i = ini;
+
+	for (int j = ini; j < fin; ++j) {
+		if (v[j] <= x) {
+			swap(v[i], v[j]);
+			i = i + 1;
+		}
+	}
+	swap(v[i], v[fin]);
+	return i;
+}
+
+template <typename T>
+
+void QuickSort(vector<T>& v, int ini, int fin) {
+	if (ini < fin) {
+		int pivot = Partition(v, ini, fin);
+		QuickSort(v, ini, pivot - 1);
+		QuickSort(v, pivot + 1, fin);
+	}
+}
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "DyV.h"
 using namespace std;
@@ -7,14 +6,77 @@ void testBusquedaBinariaInt() {
 	vector<int> valores{1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
 	int x = 7;
 	int posicion = BusquedaBinaria(x, valores, 0, valores.size() - 1);
+	cout << "Prueba con int: ";
 	if (posicion != -1) {
 		cout << "El valor " << x << " se encuentra en la posición " << posicion << endl;
 	} else {
 		cout << "No se encuentra en el array." << endl;
 	}
 }
+
+void testBusquedaBinariaFloat() {
+	vector<float> valores{1.1, 2.2, 3.3, 4.4, 5.5};
+	float x = 3.3;
+	int posicion = BusquedaBinaria(x, valores, 0, valores.size() - 1);
+	cout << "Prueba con float: ";
+	if (posicion != -1) {
+		cout << "El valor " << x << " se encuentra en la posición " << posicion << endl;
+	} else {
+		cout << "No se encuentra en el array." << endl;
+	}
+}
+
+void testBusquedaBinariaChar() {
+	vector<char> valores{'a', 'c', 'e', 'g', 'i'};
+	char x = 'e';
+	int posicion = BusquedaBinaria(x, valores, 0, valores.size() - 1);
+	cout << "Prueba con char: ";
+	if (posicion != -1) {
+		cout << "El valor '" << x << "' se encuentra en la posición " << posicion << endl;
+	} else {
+		cout << "No se encuentra en el array." << endl;
+	}
+}
+
+
+void testQuickSortInt() {
+	vector<int> valores = {9, 4, 8, 3, 1, 2, 5, 7, 6};
+	QuickSort(valores, 0, valores.size() - 1);
+	cout << "Vector de enteros ordenado: ";
+	for (int val : valores) {
+		cout << val << " ";
+	}
+	cout << endl;
+}
+
+void testQuickSortFloat() {
+	vector<float> valores = {9.5, 4.1, 8.3, 3.7, 1.9, 2.4, 5.5, 7.6, 6.2};
+	QuickSort(valores, 0, valores.size() - 1);
+	cout << "Vector de floats ordenado: ";
+	for (float val : valores) {
+		cout << val << " ";
+	}
+	cout << endl;
+}
+
+void testQuickSortChar() {
+	vector<char> valores = {'z', 'b', 'k', 'd', 'a', 'x', 'm', 'q', 'p'};
+	QuickSort(valores, 0, valores.size() - 1);
+	cout << "Vector de chars ordenado: ";
+	for (char val : valores) {
+		cout << val << " ";
+	}
+	cout << endl;
+}
+
 int main() {
 	testBusquedaBinariaInt();
+	testBusquedaBinariaFloat();
+	testBusquedaBinariaChar();
+	cout << "Pruebas de QuickSort en diferentes tipos de vectores:" << endl;
+	testQuickSortInt();
+	testQuickSortFloat();
+	testQuickSortChar();
 	return 0;
 }
 
